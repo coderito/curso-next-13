272742efaaeafcf4926021ab7560e98f56479412
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-04-20 12:31:08 -0400

    consumiendo api async / await

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,17 @@
-function Dashboard() {
-  return (
-    <div>dashboard</div>
-  )
+import Image from "next/image";
+
+async function fetchData() {
+  const response = await fetch("https://rickandmortyapi.com/api/character/3");
+  return response.json();
 }
 
+async function Dashboard() {
+  const data = await fetchData();
+
+  return (
+    <div>
+      <Image src={data.image} width={300} height={300} alt="Morty" />
+    </div>
+  );
+}
 export default Dashboard;
